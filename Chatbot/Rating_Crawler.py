ef1f82dd7cfdc54297d2cd9170265b07438c936d
isabelleKirby <wttkirby@gmail.com>
2023-04-03 22:19:49 -0500

    Charbot progress

--- Chatbot/Rating_Crawler.py
+++ Chatbot/Rating_Crawler.py
@@ -1,16 +1,22 @@
-import os
 import re
-import urllib.request
-
-import html5lib
 import requests
 from bs4 import BeautifulSoup
-from nltk.tokenize import sent_tokenize
+
 
 if __name__ == '__main__':
     url = 'https://www.imdb.com/title/tt4642970/reviews'
     r = requests.get(url)
 
     soup = BeautifulSoup(r.content, 'html5lib')
-    ratings = soup.find('div', attrs = {'class':'text show-more__control'})
-    print(type(ratings))
+    ratings = soup.findAll('div', attrs = {'class':'text show-more__control'})
+    with open('llama_ratings.txt', 'w', encoding='utf-8') as wfile:
+        for rate in ratings:
+            rate = rate.__str__()
+            rate = re.sub('<div class="text show-more__control">', ' ', str(rate))
+            rate = re.sub('<br/><br/>Tmelapse<br/><br/>', ' ', rate)
+            rate = re.sub('<br/><br/>', ' ', rate)
+            rate = re.sub('\n', ' ', rate)
+            rate = re.sub('\n', ' ', rate)
+            rate = re.sub('</div>', '\n', rate)
+            wfile.write(rate)
+        wfile.close()
